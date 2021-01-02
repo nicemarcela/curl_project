1165dc0d2713e4984059709fc818092a4a303c20
Marcela Almeida <58824632+nicemarcela@users.noreply.github.com>
2021-01-02 15:31:41 +0100

    up

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,11 @@
 module.exports = {
+  pathPrefix: "/curl_project",
   siteMetadata: {
     title: `What The Curl`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
+  
   plugins: [
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
